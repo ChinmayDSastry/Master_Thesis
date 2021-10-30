8c63880c0ad1d99249ca934963d9300ceba955cb
Chinmay <60548951+chinmay@users.noreply.github.com>
2021-10-30 09:21:51 +0200

    Generating training.txt

---
+++ Object_detection/generate_train_txt.py
@@ -0,0 +1,49 @@
+
+import os
+
+full_path_to_images = r'E:\MSC-THESIS\expt2_withAUG_SailboatJetty\synth'
+
+
+os.chdir(full_path_to_images)
+
+
+p = []
+
+
+for current_dir, dirs, files in os.walk('.'):
+    # Going through all files
+    for f in files:
+        # Checking if filename ends with '.jpeg'
+        # Change it to .png if the images are in .png format
+        if f.endswith('.png'):
+            # Preparing path to save into train.txt file
+            # If you're using Windows, it might need to change
+            # this: + '/' +
+            # to this: + '\' +
+            # or to this: + '\\' +
+            path_to_save_into_txt_files = '/content/drive/MyDrive/yolo_custom_model_Training/darknet/data/sailboat_fake' + '/' + f
+
+            # Appending the line into the list
+            # when writing lines into txt files
+            p.append(path_to_save_into_txt_files + '\n')
+
+
+
+"""
+End of:
+Getting list of full paths to labelled images
+"""
+
+
+"""
+Start of:
+Creating train.txt 
+"""
+
+# Creating file train.txt
+with open('train.txt', 'w') as train_txt:
+    # Going through all elements of the list
+    for e in p:
+        # Writing current path at the end of the file
+        train_txt.write(e)
+
